b7449722b60618fd5a28d6899951fffddd5613a7
amit.kumar@gupshup.io <amit.kumar@gupshup.io>
2021-09-16 09:10:50 +0530

    layout is added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import './App.css';
-import {Button} from 'react-bootstrap'
-import { Entry } from './pages/entry/Entrypage';
+import { DefaultLayout } from './layout/DefaultLayout';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>https://youtu.be/yiO8qXQ4BIc?list=PLtPNAX49WUFN8yq2vEuAY6AhM5EJOXQQ0</p>
-       {/* <h1>Hello CRM</h1>
-       <Button>test</Button> */}
-
-       <Entry />
-       
-      </header>
+      {/* <p>https://youtu.be/yiO8qXQ4BIc?list=PLtPNAX49WUFN8yq2vEuAY6AhM5EJOXQQ0</p> */}
+      {/* <Entry /> */}
+      <DefaultLayout >
+        //DAshboaRD HEre
+        <br/>
+        send page components
+      </DefaultLayout>
     </div>
   );
 }
---
+++ src/layout/DefaultLayout.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Footer } from './partials/Footer.compo'
+import { Header } from './partials/Header.compo'
+// import './'
+
+export const DefaultLayout = ({children}) => {
+    return (
+        <div>
+            <div className="default-layout">
+                <header className="header">
+                    <Header />
+                </header>
+                <main className="main">
+                    {/* <h1>subjects to be added to main page frontend</h1> */}
+                    {children}
+                </main>
+                <footer className="footer">
+                    <Footer />
+                </footer>
+            </div>
+        </div>
+    )
+}
